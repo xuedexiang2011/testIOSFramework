0fa5591f116fca1cbf7333c5ea46003c77b859b1
xdxiang <dexiang_xue@163.com>
2014-08-14 17:47:25 +0800

    创建测试工程

---
+++ ToolFramework/ToolFramework.h
@@ -0,0 +1,15 @@
+//
+//  ToolFramework.h
+//  ToolFramework
+//
+//  Created by x-Apple on 14-8-14.
+//  Copyright (c) 2014年 x-Apple. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ToolFramework : NSObject
+
++ (void)frameworkTest;
+
+@end
---
+++ ToolFramework/ToolFramework.m
@@ -0,0 +1,18 @@
+//
+//  ToolFramework.m
+//  ToolFramework
+//
+//  Created by x-Apple on 14-8-14.
+//  Copyright (c) 2014年 x-Apple. All rights reserved.
+//
+
+#import "ToolFramework.h"
+
+@implementation ToolFramework
+
++ (void)frameworkTest
+{
+    NSLog(@"太棒了，你是如此成功的完成了pods私有库的管理！！！真给力！！！");
+}
+
+@end
